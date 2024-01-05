0037137389ccbdda060c6417c8734f33f61b0941
EGGeorgieva21 <EGGeorgieva21@codingburgas.bg>
2024-01-06 00:27:59 +0200

    Layout p1

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include 'functions.cpp'
+using namespace std;
+
+
+struct Person
+{
+    string Name;
+    int age;
+    int socialSecurityNumber;
+    int personalNumber;
+};
+
+// block // + // block //
+
+// 0. visual gluposti tam shte pishem?????
+// 0.5 Brief description of services
+// 1. log in?
+// 2. input your assets and who you want them to go to
+// 3. input personal information? - Personall account space? // this will help us find your corpse :D
+// 4. Log out.
+// 5. Congrats you've been scammed :)
+
+// how do wills work
+int main()
+{
+
+
+}
