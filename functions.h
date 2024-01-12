be090594b00eb9ce83167151405e4c3c7c282904
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2024-01-12 22:23:38 +0200

    Logout function, printwill function , createWill, getInfo function progress. New structure person, new node list, two new functions for the node list.

--- functions.h
+++ functions.h
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
 using namespace std;
 
-
-struct Data
+struct Register
 {
     string username;
     string password;
@@ -11,14 +11,43 @@ struct Data
 
 struct Person
 {
-    string Name;
-    int age;
-    int socialSecurityNumber;
-    int personalNumber;
-};
+    string name;
+    string surname;
+    string date;
 
-Data registerData;
-Data loginData;
+    string beneficiaries[100];
+    string executor;
+    string uniqueSignature;
+}
+
+struct NODE 
+{
+    string item;
+    string recipent;
+    NODE* next;
+}
+void prependNode(NODE *&head, string item, string recipent)
+{
+    NODE* newNode = new NODE;
+    newNode->next = head;
+    newNode->item = item;
+    newNode->recipent = recipent;
+    head = newNode;
+}
+void printList(NODE *head)
+{
+    NODE* temp = head;
+    while(temp != NULL)
+    {
+        cout << "I bequest " << temp->item << " to beneficiary " << temp->recipient << endl;
+        temp = temp->next;
+    }
+}
+
+Register registerData;
+Register loginData;
+Person user;
+int numberBeneficiaries;
 
 bool containsDigit(string str)
 {
@@ -30,19 +59,21 @@ bool containsDigit(string str)
         }
     }
     return false;
-}
+} 
 
 void registering()
 {
+    cout << "Welcome to the Digital Will!" << endl;
+
     cout << "Register yourself." << endl;
     cout << endl;
   
-    cout << "Your username must contain \"codingburgas.bg\".Enter your username : ";
-    cin >> registerData.username;
+    cout << "Your username must contain \"codingburgas.bg\".Enter your username: ";
+    getline(cin, registerData.username);
     cout << endl;
 
     cout << "Your password must contain a number. Enter your password: ";
-    cin >> registerData.password;
+    getline(cin, registerData.password);
     cout << endl;
 }
 
@@ -63,10 +94,10 @@ void checkRequires()
             cout << "Your data doesn't match our requires. Try again registering." << endl;
             cout << endl;
             cout << "Your username must contain \"codingburgas.bg\".Enter your username : ";
-            cin >> registerData.username;
+            getline(cin, registerData.username);
             cout << endl;
             cout << "Your password must contain a number. Enter your password: ";
-            cin >> registerData.password;
+            getline(cin, registerData.password);
         }
     }
 }
@@ -76,10 +107,10 @@ void login()
     while (true)
     {
         cout << "Enter your username: ";
-        cin >> loginData.username;
+        getline(cin, loginData.username);
         cout << endl;
         cout << "Enter your password: ";
-        cin >> loginData.password;
+        getline(cin, loginData.password);
 
         cout << "Checking if there is an email like this..." << endl;
         cout << endl;
@@ -89,6 +120,10 @@ void login()
             if (loginData.password == registerData.password)
             {
                 cout << "Welcome " << loginData.username << "! You can continue working." << endl;
+
+                //Makes a NODE list for the user.
+                NODE* head = new NODE;
+
                 break;
             }
         }
@@ -97,4 +132,111 @@ void login()
             cout << "There is no username like this! Try again." << endl;
         }
     }
+}
+
+void getInfo() 
+{
+    cout << "The first part of making a CV is writing your personal information. Please answer the questions below." << endl;
+
+    cout << "Enter name: ";
+    getline(cin, user.name);
+    cout << endl;
+
+    cout << "Enter surname: ";
+    getline(cin, user.surname);
+    cout << endl;
+
+    cout << "Enter the current date: ";
+    cin >> user.data;
+    cout << endl;
+
+    cout << "Enter the number of beneficiaries: "
+    cin >> numberBeneficiaries;
+	cout << "\nEnter beneficiary: ";
+    for (int i = 0; i < numberBeneficiaries; i++)
+    {	
+        cin >> user.beneficiaries[i];
+    }
+    cout << endl;
+
+    cout << "Enter the executor of your will. ";
+    cin >> user.executor;
+    cout << endl;
+
+    cout << "Lastly, enter your signature. This will be used to sign your will. ";
+    cin >> user.uniqueSignature;
+    cout << endl;
+}
+
+void createWill() 
+{
+    string item;
+    cout << "Please enter your asset/s or item/s: ";
+    getline(cin, item);
+
+    string recipient;
+    cout << "The intended recipent: ";
+    getline(cin, recipient);
+
+    cout << "I bequeath " << item << " to " << recipient << ". Is this correct? y/n\n";
+
+    string answer;
+    getline(cin, answer);
+    if(string == "yes" || string == "y" || string == "Yes")
+    {
+        prependNode(head, item, recipent);
+		cout << "\nAsset has been added. Continue adding assets? y/n";
+        cin >> answer;
+		if (string == "yes" || string == "y" || string == "Yes")
+        {
+            cout << endl;
+            createWill();
+        }
+    }else{
+        cout << "\nAsset has not been added. Continue adding assets? y/n";
+		if (string == "yes" || string == "y" || string == "Yes")
+		{
+			cout << endl;
+			createWill();
+		}
+    }
+    
+  
+void printWill()
+{
+    // the below code may or may not work
+    cout << "\033[2J\033[H"; // ANSI escape codes to clear the console
+
+    cout << "Your will is complete. Here it is: " << endl;
+    
+
+    cout << "      Name: " << user.name << endl;
+
+    cout << "      Surname: " << user.surname << endl;
+
+    cout << "      Date:" << user.date << endl;
+
+    cout << "      Beneficiaries: ";
+    for (int i = 0; i < numberBeneficiaries; i++)
+    {
+        cout << user.beneficiaries[i] << " ";
+    }
+    cout << endl;
+    cout << "      Executor: " << user.executor << endl;
+
+
+    cout << "This is my last will and testament. " << endl
+    printList(head);
+
+    cout << "Signed, " << user.uniqueSignature;
+
+
+}
+
+void logout()
+{
+    cout << "Press any key to logout...\n";
+    _getch();  // Press _getch() to logout
+
+    cout << "Logout successful.\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 #include "functions.h"
+#include <conio.h>
 using namespace std;
 
-// block // + // block //
-
-// 0. visual gluposti tam shte pishem?????
-// 0.5 Brief description of services
-// 1. log in?
-// 2. input your assets and who you want them to go to
-// 3. input personal information? - Personall account space? // this will help us find your corpse :D
-// 4. Log out.
-// 5. Congrats you've been scammed :)
+// 3. input personal information? - Personall account space? // this will help us find your corpse
 
 // how do wills work
 // There are two types of wills - 1. Bulgarian Handwritten Will (this is the one we'll be making) 2. Notarial Will
 // - the text of the will must be kept secret, and also, there must be a date. Only the latest will is valid.
 // requirements of the Bulgarian Legislation? - must be signed with full name and signature
-// small end text - we recommend registering this will at the Notary office.
-
-// change logo to codders
+// small end text - w
+//e recommend registering this will at the Notary office.
 
 
 int main()
 {
-    int a;
-    cin >> a;
-    cout << a;
     registering();
     checkRequires();
     login();
+    getInfo();
 
+    //cout << "In the following, please put the contents of your will and their intended recipent/s. " << endl;
+    createWill();
+    printWill();
+    logout();
 }
